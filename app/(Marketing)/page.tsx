cfc123218434f4ee733354e6a6ae5bdf2aafa9db
loweceo <leonkone@outlook.fr>
2023-11-06 17:26:38 +0100

    last ok

--- app/(Marketing)/page.tsx
+++ app/(Marketing)/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <Link href="/dashboard">go to dashboard</Link>
+        <Link href="/dashboard" className="bg-white text-black px-3 py-1.5 rounded-lg hover:bg-white/90">go to dashboard</Link>
       </div>
     </main>
   );
